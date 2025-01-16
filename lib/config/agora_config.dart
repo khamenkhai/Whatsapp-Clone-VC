2fa5b54b5a5e6d7e8232eaccd6f7fada742cdb28
Kham En Khai <khamenkhai2003@gmail.com>
2025-01-16 16:40:32 +0630

    update app id

--- lib/config/agora_config.dart
+++ lib/config/agora_config.dart
@@ -1,5 +1,5 @@
 class AgoraConfig {
   static String token = '';
-  static String appId = '01254a6c76514e4787628f4b6bdc1786';
-  static String appcertificate = 'd4cafc0cc7d3466d9b51b9d604d971ff';
+  static String appId = '00fb2493916544ce84340c82899ce0b5';
+  static String appcertificate = 'b4b801b7107f4bd59150894664ac56f2';
 }
